49d1bca50b521dbc8d11a6849d60bfdada8b7668
Alastaray <rikinikto@gmail.com>
2022-02-16 19:28:24 +0200

    Add a opportunity to download picture by url.

--- endpoint/Picture.cs
+++ endpoint/Picture.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -7,8 +8,20 @@ using System.Threading.Tasks;
 
 namespace Project
 {
+    public class PictureExpection : System.Exception
+    {
+        public PictureExpection(string message, int status_code) : base(message)
+        {
+            StatusCode = status_code;
+        }
+        public int StatusCode { get; set; }
+    }
+
     public class Picture
     {
+        public string Url { get; set; }
+        public string Path { get; set; }
+
         public bool SetUrl(HttpListenerRequest request)
         {
             if (request.QueryString.Count != 0)
@@ -22,17 +35,36 @@ namespace Project
                     }
                 }
             }
-            return false;
+            throw new PictureExpection("Incorrect url!", 400);
         }
         public bool DownloudPicture(HttpListenerRequest request)
         {
             if (Url == null)
             {   
-                if(!SetUrl(request))return false;    
+                if(!SetUrl(request)) throw new PictureExpection("Incorrect url!", 400);    
             };
+            try
+            {
+                WebClient client = new WebClient();
+                string[] names = Url.Split(new char[] { '/' });//"Images/" +
+                string filename = names[names.Length - 1];
+                try
+                {                                   
+                    Path = "Images//" + filename;
+                    client.DownloadFile(Url, Path);
+                }
+                catch (WebException)
+                {
+                    Path = filename;
+                    client.DownloadFile(Url, Path);
+                }               
+            }
+            catch (Exception e)
+            {
+                throw new PictureExpection(Path, 400);
+            }
             return true;
         }
-        public string Url { get; set; }
-        public string Path { get; set; }
+        
     }
 }
--- endpoint/Program.cs
+++ endpoint/Program.cs
@@ -5,15 +5,17 @@ using System.Net.Http.Json;
 
 namespace Project
 {
-
-    public class Error
+    public class ErrorMessage
     {
-        public Error(string what)
-        {
-            error = what;
-        }
-        public string error { get; }
+        public ErrorMessage(string message) { Error = message; }
+        public string Error { get; set; }
     }
+    public class Link
+    {
+        public Link(string url) { Url = url; }
+        public string Url { get; set; }
+    }
+
 
 
     class Program
@@ -21,23 +23,32 @@ namespace Project
         static void Main(string[] args)
         {
             HttpListener listener = new HttpListener();
-            listener.Prefixes.Add("http://localhost:8888/");
+            string host = "http://localhost:8888/";
+            listener.Prefixes.Add(host);
             listener.Start();
+            Console.WriteLine("Waiting for connection!");
             while (true)
             {
                 HttpListenerContext context = listener.GetContext();
+                HttpListenerRequest request = context.Request;
                 HttpListenerResponse response = context.Response;
                 Picture picture = new Picture();
                 JsonContent json;
-                if (picture.SetUrl(context.Request))
+                try
                 {
-                    json = JsonContent.Create(picture);
+                    picture.DownloudPicture(request);
+                    json = JsonContent.Create(new Link(host+picture.Path));
                 }
-                else
+                catch (PictureExpection er)
+                {
+                    response.StatusCode = er.StatusCode;
+                    json = JsonContent.Create(new ErrorMessage(er.Message));
+                }
+                catch (Exception er)
                 {
                     response.StatusCode = 400;
-                    json = JsonContent.Create(new Error("Incorrect url!"));
-                }            
+                    json = JsonContent.Create(new ErrorMessage(er.Message));
+                }                     
                 Stream output = response.OutputStream;
                 json.CopyToAsync(output);
                 output.Close();
