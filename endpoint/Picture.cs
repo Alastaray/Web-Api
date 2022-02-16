9bfa70e74a2506850874abf1ead216d92a457a76
Alastaray <rikinikto@gmail.com>
2022-02-16 18:15:36 +0200

    initial commit

---
+++ endpoint/Picture.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    public class Picture
+    {
+        public bool SetUrl(HttpListenerRequest request)
+        {
+            if (request.QueryString.Count != 0)
+            {
+                foreach (string key in request.QueryString.Keys)
+                {
+                    if (key == "url")
+                    {
+                        Url = request.QueryString.Get(key);
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        public bool DownloudPicture(HttpListenerRequest request)
+        {
+            if (Url == null)
+            {   
+                if(!SetUrl(request))return false;    
+            };
+            return true;
+        }
+        public string Url { get; set; }
+        public string Path { get; set; }
+    }
+}
---
+++ endpoint/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace Project
+{
+
+    public class Error
+    {
+        public Error(string what)
+        {
+            error = what;
+        }
+        public string error { get; }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:8888/");
+            listener.Start();
+            while (true)
+            {
+                HttpListenerContext context = listener.GetContext();
+                HttpListenerResponse response = context.Response;
+                Picture picture = new Picture();
+                JsonContent json;
+                if (picture.SetUrl(context.Request))
+                {
+                    json = JsonContent.Create(picture);
+                }
+                else
+                {
+                    response.StatusCode = 400;
+                    json = JsonContent.Create(new Error("Incorrect url!"));
+                }            
+                Stream output = response.OutputStream;
+                json.CopyToAsync(output);
+                output.Close();
+            }
+            listener.Stop();
+        }
+    }
+}
